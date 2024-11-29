5fd4aebabde1020535d5d60a43a50635e79649cf
DavidMayorgaS08 <david08102@hotmail.com>
2024-11-28 21:08:08 -0500

    Cambio logica modificar

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -39,9 +39,19 @@ const usuariosControllers = {
     // Actualizar usuarios
     async putUsuario(req, res) {
         try {
-            req.body.Password = bcrypt.hashSync(req.body.Password, 10);
-            const usuario = await Usuarios.findByIdAndUpdate(req.params.id, req.body, { new: true });
-            res.json(usuario);
+            const usuarioActual = await Usuarios.findById(req.params.id);
+            if (!usuarioActual) {
+                return res.status(404).json({ message: 'Usuario no encontrado' });
+            }
+    
+            if (req.body.Password && req.body.Password !== usuarioActual.Password) {
+                req.body.Password = bcrypt.hashSync(req.body.Password, 10);
+            } else {
+                req.body.Password = usuarioActual.Password;
+            }
+    
+            const usuarioActualizado = await Usuarios.findByIdAndUpdate(req.params.id, req.body, { new: true });
+            res.json(usuarioActualizado);
         } catch (error) {
             res.status(500).json({
                 message: error.message
