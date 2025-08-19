6f73eeae7de3de1fec49f64ac0c38927be3124a4
DavidMayorgaS08 <david08102@hotmail.com>
2025-08-19 14:47:01 -0500

    Ajustes

--- models/climas.js
+++ models/climas.js
@@ -2,12 +2,12 @@ import mongoose from 'mongoose';
 
 const climasSchema = new mongoose.Schema({
     Id_finca: { type: mongoose.Schema.Types.ObjectId, ref: 'Fincas', required: true },
-    Temperatura: { type: Number, required: true },
+    Temperatura: { type: String, required: true },
     Clima: { type: String, required: true },
     Fecha: { type: Date, required: true },
-    Humedad: { type: Number, required: true },
-    Velocidad_viento: { type: Number, required: true },
-    Nubosidad: { type: Number, required: true }
+    Humedad: { type: String, required: true },
+    Velocidad_viento: { type: String, required: true },
+    Nubosidad: { type: String, required: true }
 });
 
 export default mongoose.model('Climas', climasSchema);
